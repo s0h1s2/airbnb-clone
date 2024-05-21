525900447277ea52438c38ac30ba2123094a825c
shkar <shkarhsardar@gmail.com>
2024-05-21 21:59:41 +0300

    fix

--- client/src/lib/client.ts
+++ client/src/lib/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiBase = import.meta.env.API_BASE_URL
+const apiBase = import.meta.env.VITE_API_BASE_URL
 
 export const client = axios.create({
   baseURL: apiBase,
